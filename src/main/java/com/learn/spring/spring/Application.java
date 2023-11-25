e61b64f79539410cf34345dcc194e145a54549d6
Aakash <aakashchaturvedi7@gmail.com>
2023-11-25 20:53:44 +0100

    wip

--- src/main/java/com/learn/spring/spring/Application.java
+++ src/main/java/com/learn/spring/spring/Application.java
@@ -3,6 +3,8 @@ package com.learn.spring.spring;
 import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 
@@ -19,28 +21,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
-	}
-
-	private void isRunningUp() throws IOException {
-		isServiceUp();
-	}
-
-	private boolean isServiceUp() throws IOException {
-		String address = env.getProperty("service1.address");
-		String port = env.getProperty("service1.port");
-		return isServiceReachable(address, Integer.parseInt(port), 3000);
-	}
-
-
-	private static boolean isServiceReachable(String address, int port, int timeout) throws IOException {
-		Socket socket = new Socket();
-		try {
-			socket.connect(new InetSocketAddress(address, port), timeout);
-			return  true;
-		} catch( IOException e) {
-			return false;
-		}finally {
-			socket.close();
-		}
 	}
 }
--- src/main/java/com/learn/spring/spring/exception/CloudVendorNotFoundException.java
+++ src/main/java/com/learn/spring/spring/exception/CloudVendorNotFoundException.java
@@ -10,6 +10,4 @@ public class CloudVendorNotFoundException extends RuntimeException {
         super(message, throwable);
     }
 
-
-
 }

d5c23460d745b6f0f48b1c52e70eb76b7e09812d
Aakash <aakashchaturvedi7@gmail.com>
2023-11-26 22:00:36 +0100

    Added Automatic reload dependency, refactored gradle

--- src/main/java/com/learn/spring/spring/Application.java
+++ src/main/java/com/learn/spring/spring/Application.java
@@ -1,16 +1,9 @@
 package com.learn.spring.spring;
 
-import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
-
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Socket;
 
 @SpringBootApplication
 public class Application {
